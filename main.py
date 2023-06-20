c10d3b37ff550d3b07e9d487627715ee341ec275
Yan Khachko <a@slnk.icu>
2023-06-20 19:29:55 +0400

    Cloudfront throw proxy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import Response, StreamingResponse
+from urllib.parse import urlparse
 
-import xml.etree.ElementTree as ET
+from typing import Optional
+
+from lxml import etree
 
 import requests
 import os
@@ -10,39 +13,128 @@ app = FastAPI()
 
 OLD_BASE = os.environ.get("OLD_BASE", "https://anchor.fm/")
 NEW_BASE = os.environ.get("NEW_BASE", "https://podcast.sotaproject.com/")
+CLOUDFRONT_PREFIX = os.environ.get("CLOUDFRONT_PREFIX", "/staging/podcast_uploaded_")
+TOKEN = os.environ.get("TOKEN", None)
 
 
-def replace_enclosure_urls(xml_string):
+def validate_url(url):
+    parsed_url = urlparse(url)
+
+    path = parsed_url.path
+
+    if not path.lower().startswith(CLOUDFRONT_PREFIX):
+        return False
+
+    domain = parsed_url.netloc.lower()
+    allowed_domain = ".cloudfront.net"
+
+    if not domain.endswith(allowed_domain):
+        return False
+
+    file_extension = os.path.splitext(path)[1][1:]
+
+    if file_extension.lower() not in ["jpg", "png", "jpeg", "webp"]:
+        return False
+
+    return True
+
+
+def replace_urls(xml_string):
     # Parse the XML string
-    root = ET.fromstring(xml_string)
+    root = etree.fromstring(xml_string)
 
     # Find all <enclosure> tags and replace the URL attribute
-    for enclosure in root.iter("enclosure"):
+    for enclosure in root.xpath("//enclosure"):
         enclosure.attrib["url"] = enclosure.attrib["url"].replace(OLD_BASE, NEW_BASE)
 
-    # Generate the modified XML string
-    modified_xml_string = ET.tostring(root, encoding="utf-8").decode()
-
-    return modified_xml_string
-
-
-@app.get("/{url:path}")
-async def proxy(url: str):
-    response = requests.get(OLD_BASE + url, allow_redirects=True)
-
-    content = response.content
-
-    headers = {}
-
-    if (
-        response.headers.get("content-type", "application/plaintext")
-        == "application/rss+xml; charset=utf-8"
+    # Find all <atom:link> tags and replace the href attribute
+    for link in root.xpath(
+        "//atom:link", namespaces={"atom": "http://www.w3.org/2005/Atom"}
     ):
-        content = replace_enclosure_urls(content)
+        link.attrib["href"] = link.attrib["href"].replace(OLD_BASE, NEW_BASE)
 
-        headers["content-type"] = "application/rss+xml"
+    # Find all <itunes:image> tags and replace the href attribute
+    for link in root.xpath(
+        "//itunes:image",
+        namespaces={"itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd"},
+    ):
+        if not validate_url(link.attrib["href"]):
+            continue
+        link.attrib["href"] = (
+            NEW_BASE
+            + f'cloudfront?{("token=" + TOKEN + "&") if TOKEN is not None else ""}url='
+            + link.attrib["href"]
+        )
 
-    return Response(content, headers=headers)
+    # Find all <url> tags inside <image> tags and replace the text
+    for url in root.xpath("//image/url"):
+        if not validate_url(url.text):
+            continue
+        url.text = (
+            NEW_BASE
+            + f'cloudfront?{("token=" + TOKEN + "&") if TOKEN is not None else ""}url='
+            + url.text
+        )
+
+    # Generate the modified XML string
+    return etree.tostring(root, encoding=str)
+
+
+@app.get("/cloudfront")
+async def cloudfront(url: str, token: str | None = None):
+    if TOKEN is not None and token != TOKEN:
+        raise HTTPException(401)
+
+    if not validate_url(url):
+        raise HTTPException(403)
+
+    # Make the request using requests.get
+    response = requests.get(url, stream=True)
+
+    headers = {
+        "content-type": response.headers.get("content-type", "application/plaintext")
+    }
+
+    # Define a generator function to stream the response content
+    def stream_generator():
+        for chunk in response.iter_content(chunk_size=1024):
+            yield chunk
+
+    # Create a StreamingResponse and return it
+    return StreamingResponse(stream_generator(), headers=headers)
+
+
+@app.get("/{path:path}")
+async def proxy(path: str, token: str | None = None):
+    if TOKEN is not None and token != TOKEN:
+        raise HTTPException(401)
+
+    url = OLD_BASE + path
+    response = requests.head(url, allow_redirects=True)
+
+    headers = {
+        "content-type": response.headers.get("content-type", "application/plaintext")
+    }
+
+    if headers["content-type"] == "application/rss+xml; charset=utf-8":
+        response = requests.get(url, allow_redirects=True)
+
+        content = response.content
+
+        content = replace_urls(content)
+
+        return Response(content, headers=headers)
+
+    # Make the request using requests.get
+    response = requests.get(url, stream=True)
+
+    # Define a generator function to stream the response content
+    def stream_generator():
+        for chunk in response.iter_content(chunk_size=1024):
+            yield chunk
+
+    # Create a StreamingResponse and return it
+    return StreamingResponse(stream_generator(), headers=headers)
 
 
 if __name__ == "__main__":
